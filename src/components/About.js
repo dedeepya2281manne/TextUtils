6b27f679a7400965769f30d635a08e13ce948029
MANNE DEDEEPYA <dedeepyamanne02@gmail.com>
2023-12-23 17:42:57 +0530

    modified few corrections

--- src/components/About.js
+++ src/components/About.js
@@ -3,24 +3,24 @@ import React, {useState} from 'react'
 export default function About() {
 
     const [myStyle, setMyStyle] = useState({
-        color: 'white',
-        backgroundColor: 'black'
+        color: 'black',
+        backgroundColor: 'white'
     });
-    const [btnText, setBtnText] = useState("Enable Light Mode");
+    const [btnText, setBtnText] = useState("Enable Dark Mode");
     const toggleStyle = () =>{
-        if(myStyle.color === 'white'){
-            setMyStyle({
-                color: 'black',
-                backgroundColor: 'white'
-            });
-            setBtnText("Enable Dark Mode");
-        }
-        else{
+        if(myStyle.color === 'black'){
             setMyStyle({
                 color: 'white',
                 backgroundColor: 'black'
-            })
+            });
             setBtnText("Enable Light Mode");
+        }
+        else{
+            setMyStyle({
+                color: 'black',
+                backgroundColor: 'white'
+            })
+            setBtnText("Enable Dark Mode");
         }
 
         
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,28 +5,48 @@ export default function TextForm(props) {
     const [text, setText] = useState('');
     const handleUpClick = () =>{
         let newText = text.toUpperCase();
+        if(text.split(" ").filter((element)=>{
+            return element.length!==0;
+        }).length){
         props.showAlert("Text converted to UpperCase","success");
+        }
         setText(newText);
     }
     const handleLowClick = () =>{
         let newText = text.toLowerCase();
-        props.showAlert("Text converted to LowerCase","success");
+        if(text.split(" ").filter((element)=>{
+            return element.length!==0;
+        }).length){
+            props.showAlert("Text converted to LowerCase","success");
+        }
         setText(newText);
     }
     const handleClear = () =>{
         let newText = "";
         setText(newText);
-        props.showAlert("Text is cleared","success");
+        if(text.split(" ").filter((element)=>{
+            return element.length!==0;
+        }).length){
+            props.showAlert("Text is cleared","success");
+        }
     }
     const handleCopyText = () =>{
-        var text = document.getElementById('myBox');
-        navigator.clipboard.writeText(text.value);
-        props.showAlert("Text is copied","success");
+        var data = document.getElementById('myBox');
+        navigator.clipboard.writeText(data.value);
+        if(text.split(" ").filter((element)=>{
+            return element.length!==0;
+        }).length){
+            props.showAlert("Text is copied","success");
+        }
     }
     const handleRemoveSpaces = () =>{
         var data = text.split(/[ ]+/);
         setText(data.join(" "));
-        props.showAlert("Extra spaces removed","success");
+        if(text.split(" ").filter((element)=>{
+            return element.length!==0;
+        }).length){
+            props.showAlert("Extra spaces removed","success");
+        }
     }
     const handleOnChange = (event) =>{
         setText(event.target.value);
@@ -38,18 +58,22 @@ export default function TextForm(props) {
                 <div className="mb-3">
                     <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
                 </div>
-                <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to UpperCase</button>
-                <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to LowerCase</button>
-                <button className="btn btn-primary mx-1" onClick={handleCopyText}>Copy Text</button>
-                <button className="btn btn-primary mx-1" onClick={handleRemoveSpaces}>Remove Extra Sapces</button>
-                <button className="btn btn-primary mx-1" onClick={handleClear}>Clear Text</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to UpperCase</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleLowClick}>Convert to LowerCase</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCopyText}>Copy Text</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleRemoveSpaces}>Remove Extra Sapces</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleClear}>Clear Text</button>
             </div>
             <div className="container my-3">
                 <h1>Your text summary</h1>
-                <p>{text.split(" ").length} words and {text.length} characters</p>
-                <p>{0.008 * text.split(" ").length} Minutes read</p>
+                <p>{text.split(" ").filter((element)=>{
+                    return element.length!==0;
+                }).length} words and {text.length} characters</p>
+                <p>{0.008 * text.split(" ").filter((element)=>{
+                    return element.length!==0;
+                }).length} Minutes read</p>
                 <h2>Preview</h2>
-                <p>{text}</p>
+                <p>{text.length>0?{text}:"Nothing to preview."}</p>
             </div>
         </>
   )
